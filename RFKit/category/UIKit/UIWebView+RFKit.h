465504d81a21e6e1c3e2c5732228811333a0cb6e
BB9z <bb9z@live.com>
2013-01-14 15:55:46 +0800

    Add new UIWebView extension.

---
+++ RFKit/category/UIKit/UIWebView+RFKit.h
@@ -0,0 +1,18 @@
+/*!
+    UIWebView extension
+    RFKit
+
+    Copyright (c) 2012-2013 BB9z
+    http://github.com/bb9z/RFKit
+
+    The MIT License (MIT)
+    http://www.opensource.org/licenses/mit-license.php
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface UIWebView (RFKit)
+
+- (void)clearBackgroundImages;
+
+@end
