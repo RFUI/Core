dbf259e3d22f102059b452df6bf68c7ddef2970e
BB9z <bb9z@live.com>
2013-01-17 10:53:12 +0800

    Add RFAlloctionProxyButton.

---
+++ RFKit/RFAlloctionProxy/RFAlloctionProxyButton.h
@@ -0,0 +1,13 @@
+//
+//  RFAlloctionProxyButton.h
+//  peixunbu
+//
+//  Created by BB9z on 13-1-16.
+//  Copyright (c) 2013年 edoctor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RFAlloctionProxyButton : UIButton
+
+@end
---
+++ RFKit/RFAlloctionProxy/RFAlloctionProxyButton.m
@@ -0,0 +1,14 @@
+//
+//  RFAlloctionProxyButton.m
+//  peixunbu
+//
+//  Created by BB9z on 13-1-16.
+//  Copyright (c) 2013年 edoctor. All rights reserved.
+//
+
+#import "RFAlloctionProxyButton.h"
+#import "RFPerformance.h"
+
+@implementation RFAlloctionProxyButton
+
+@end
