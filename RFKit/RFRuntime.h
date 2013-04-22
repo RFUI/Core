494248cac7bf295a9fa56e1940185f447cc64477
BB9z <bb9z@myopera.com>
2013-04-22 09:29:35 +0800

    More components from libextobjc. Key path coding use original version.

--- RFKit/RFRuntime.h
+++ RFKit/RFRuntime.h
@@ -30,46 +30,14 @@
 #endif
 
 #pragma mark -
-#include "metamacros.h"
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+#include "metamacros.h"
 #import "RFARC.h"
 #import "RFFeatureSupport.h"
 
 #pragma mark - ObjC Ext
-/*!
-    Ref from EXTKeyPathCoding.h (extobjc)
-    https://github.com/jspahrsummers/libextobjc
-
-    Copyright (c) 2012 - 2013 Justin Spahr-Summers
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy of
-    this software and associated documentation files (the "Software"), to deal in
-    the Software without restriction, including without limitation the rights to
-    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-    the Software, and to permit persons to whom the Software is furnished to do so,
-    subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
-/** Keypath helper support auto complete and compile-time verification.
-
- eg:
- 
- UIViewController *vc = nil;
- NSLog(@"%@", @keypath(vc.view, frame.origin));     // Will log "frame.origin"
- */
-#ifndef keypath
-    #define keypath(OBJ, PATH)\
-        (( (void)(NO && ((void)OBJ.PATH, NO)), # PATH ))
-#endif
+#import "EXTKeyPathCoding.h"
+#import "EXTScope.h"
+//#import "EXTSwizzle.h"
