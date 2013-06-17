322251145507252bedc46393086823a2d5b19ec8
BB9z <bb9z@myopera.com>
2013-06-17 13:52:34 +0800

    RFKit 1.3.

--- RFKit/RFDispatch.h
+++ RFKit/RFDispatch.h
@@ -1,3 +1,15 @@
+/*!
+    RFDispatch
+    RFKit
+
+    GCD helper
+
+    Copyright (c) 2012-2013 BB9z
+    https://github.com/bb9z/RFKit
+
+    The MIT License (MIT)
+    http://www.opensource.org/licenses/mit-license.php
+ */
 
 #import "RFRuntime.h"
 
