3aca48529e583cd98e49f2037883cd75e76911f7
BB9z <bb9z@myopera.com>
2013-06-17 10:59:17 +0800

    As dispatch_get_current_queue() was deprecated, dispatch_after_seconds() using dispatch_get_main_queue() now.

--- RFKit/RFDispatch.h
+++ RFKit/RFDispatch.h
@@ -1,6 +1,10 @@
 
 #import "RFRuntime.h"
 
+/** Short version for dispatch_after.
+ 
+ @warning The block will be executed on main thread.
+ */
 #ifdef __BLOCKS__
 __OSX_AVAILABLE_STARTING(__MAC_10_6,__IPHONE_4_0)
 DISPATCH_EXPORT DISPATCH_NONNULL2 DISPATCH_NOTHROW
--- RFKit/RFDispatch.m
+++ RFKit/RFDispatch.m
@@ -3,5 +3,5 @@
 
 void dispatch_after_seconds(NSTimeInterval delayInSeconds, dispatch_block_t block) {
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-    dispatch_after(popTime, dispatch_get_current_queue(), block);
+    dispatch_after(popTime, dispatch_get_main_queue(), block);
 }
