104952477cd4f2d5835bdff0f49c70c4c2d29a2f
BB9z <bb9z@me.com>
2013-11-25 14:05:49 +0800

    UIImage+RFKit: new method to get tined image.

--- RFKit/category/UIKit/UIImage+RFKit.h
+++ RFKit/category/UIKit/UIImage+RFKit.h
@@ -3,7 +3,7 @@
     RFKit
 
     Copyright (c) 2012-2013 BB9z
-    https://github.com/bb9z/RFKit
+    https://github.com/BB9z/RFKit
 
     The MIT License (MIT)
     http://www.opensource.org/licenses/mit-license.php
@@ -31,4 +31,33 @@
 - (UIImage*)imageWithScale:(CGFloat)scale;
 
 - (UIImage *)imageByScalingAndCroppingForSize:(CGSize)targetSize  DEPRECATED_ATTRIBUTE;
+
+#pragma mark - Tint color
+/// @name Tint color
+
+/** Creates and returns an tined image object that uses the specified color object.
+
+ This method preserve the highlights, shadows and alpha of the source image.
+ 
+ @see imageOnlyKeepsAlphaWithTintColor:
+ 
+ @param tintColor A color used to tint the receiver.
+ 
+ @return A tinted image.
+*/
+- (UIImage *)imageWithTintColor:(UIColor *)tintColor;
+
+/** Creates and returns an tined image object that uses the specified color object.
+
+ This method only preserve the alpha of the source image.
+ 
+ @see imageWithTintColor:
+ 
+ @param tintColor A color used to tint the receiver.
+ 
+ @return A tinted image.
+*/
+- (UIImage *)imageOnlyKeepsAlphaWithTintColor:(UIColor *)tintColor;
+
+
 @end
