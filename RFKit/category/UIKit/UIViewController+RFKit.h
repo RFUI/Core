99b2a22ce1dee91d1f509c6b3a5fa5a23d0ac844
BB9z <bb9z@me.com>
2014-01-06 17:03:53 +0800

    Document improves, stop using @abstract.

--- RFKit/category/UIKit/UIViewController+RFKit.h
+++ RFKit/category/UIKit/UIViewController+RFKit.h
@@ -14,8 +14,7 @@
 @interface UIViewController (RFKit)
 - (void)setNavTitle:(NSString *)title back:(NSString *)backTitle;
 
-/**
- @abstract Return a view which can present another view controller.
+/** Return a view which can present another view controller.
  
  @discussion First we get an window´s root view controller. Then  traverse its presentedViewController property until the presentedViewController property is nil.
  
@@ -23,8 +22,7 @@
  */
 + (UIViewController *)rootViewControllerWhichCanPresentModalViewController;
 
-/**
- @abstract Adds the given view controller as a child and add its view to specified view as a subview.
+/** Adds the given view controller as a child and add its view to specified view as a subview.
  
  @discussion Using this method, you dont need to call  `didMoveToParentViewController` manually.
  
@@ -33,15 +31,13 @@
  */
 - (void)addChildViewController:(UIViewController *)childController intoView:(UIView *)viewControllerSubview;
 
-/**
- @abstract Removes the receiver and its view from its parent view controller and the view´s superview.
+/** Removes the receiver and its view from its parent view controller and the view´s superview.
  
  @discussion Using this method, you dont need to call  `willMoveToParentViewController` manually.
  */
 - (void)removeFromParentViewControllerAndView;
 
-/**
- @abstract Dismiss keyboard by resign the first responder status.
+/** Dismiss keyboard by resign the first responder status.
  
  @discussion You can use this method to dismiss keyboard even the first responder not belong to the reciver.
  */
