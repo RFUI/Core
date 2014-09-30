c43fcb2aca8b44ab2c468d3fbd78cbf6d0b41a59
BB9z <bb9z@me.com>
2014-09-30 08:04:47 +0800

    RFFrameChangeProxy log bounds change.

--- RFKit/doutkit/RFFrameChangeProxy.m
+++ RFKit/doutkit/RFFrameChangeProxy.m
@@ -4,4 +4,10 @@
 @implementation RFFrameChangeProxyView
 RFFrameChangeLog
 
+- (void)setBounds:(CGRect)bounds {
+    dout(@"Change bounds from %@ to %@. Trace:", NSStringFromCGRect(self.bounds), NSStringFromCGRect(bounds));
+    [super setBounds:bounds];
+    douttrace()
+}
+
 @end
