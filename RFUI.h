84a99e2b59d8c03b1ec09da688808fd7e1706e67
BB9z <bb9z@me.com>
2018-04-21 19:55:07 +0800

    Remove deprecates.

--- RFUI.h
+++ RFUI.h
@@ -1,7 +1,7 @@
 /*!
     RFUI
  
-    Copyright (c) 2012-2014, 2017 BB9z
+    Copyright (c) 2012-2014, 2017-2018 BB9z
     https://github.com/RFUI/Core
  
     The MIT License (MIT)
@@ -11,28 +11,15 @@
 #pragma once
 
 #import "RFKit.h"
-#import "RFInitializing.h"
 
 #if TARGET_OS_IOS
 #import "RFUIInterfaceOrientationSupport.h"
-#import "RFUIDebug.h"
-#import "RFKeyboard.h"
 #endif
 
 typedef NS_ENUM(NSInteger, RFUIOrientation) {
     RFUIOrientationVertical = 0,
 	RFUIOrientationHorizontal,
 } ;
-
-typedef struct {
-	float top;
-	float right;
-	float bottom;
-	float left;
-} RFEdge;
-
-// Default is 10x.
-extern float RFUIDebugSlowAnimationsRatio;
 
 #if TARGET_OS_IOS
 /**
@@ -53,15 +40,3 @@ CG_INLINE UIEdgeInsets UIEdgeInsetsReverse(UIEdgeInsets insets) {
 extern NSUInteger const UIViewAutoresizingFlexibleSize;
 extern NSUInteger const UIViewAutoresizingFlexibleMargin;
 #endif
-
-#pragma mark - DEPRECATED
-
-bool RFEdgeEqualToEdge (RFEdge a, RFEdge b);
-RFEdge RFEdgeMake (float top, float right, float bottom, float left);
-RFEdge RFEdgeMakeWithRects (CGRect outterRect, CGRect innerRect);
-
-CG_INLINE RFEdge RFEdgeMakeWithFloat (float edge) {
-	return RFEdgeMake(edge, edge, edge, edge);
-}
-
-extern const RFEdge RFEdgeZero;
--- RFUI.m
+++ RFUI.m
@@ -1,36 +1,6 @@
 
 #import "RFUI.h"
 
-float RFUIDebugSlowAnimationsRatio = 10;
-
-bool RFEdgeEqualToEdge(RFEdge a, RFEdge b) {
-	if (a.top == b.top && a.right == b.right &&
-		a.bottom == b.bottom && a.left == b.left) {
-		return true;
-	}
-	return false;
-}
-
-RFEdge RFEdgeMake(float top, float right, float bottom, float left) {
-	RFEdge tmp;
-	tmp.top = top;
-	tmp.right = right;
-	tmp.bottom = bottom;
-	tmp.left = left;
-	return tmp;
-}
-
-RFEdge RFEdgeMakeWithRects(CGRect outterRect, CGRect innerRect) {
-	RFEdge tmp;
-	tmp.top = CGRectGetMinY(innerRect) - CGRectGetMinY(outterRect);
-	tmp.right = CGRectGetMaxX(outterRect) - CGRectGetMaxX(innerRect);
-	tmp.bottom = CGRectGetMaxY(outterRect) - CGRectGetMaxY(innerRect);
-	tmp.left = CGRectGetMinX(innerRect) - CGRectGetMinX(outterRect);
-	return tmp;
-}
-
-const RFEdge    RFEdgeZero = {0,0,0,0};
-
 #pragma mark - UIViewAutoresizing
 #if TARGET_OS_IOS
 NSUInteger const UIViewAutoresizingFlexibleSize = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
